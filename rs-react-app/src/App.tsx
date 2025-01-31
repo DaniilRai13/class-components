cd10d37a83d0daee1b37c4814d377d92604be267
DaniilRai13 <danik.ray.23@mail.ru>
2025-01-31 18:44:31 +0300

    feat: create function to add result of request

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,11 +1,11 @@
 import { Component } from 'react'
 import styles from './App.module.scss'
 import Header from './components/Header/Header'
-import Result from './components/Result/Result'
+import Main from './components/Main/Main'
+import { ApiResponse } from './types/resultAPI.interface'
 
 interface IState {
-  searchTerm: string
-  results: []
+  result: ApiResponse | null
   isLoading: false
   error: string | null
 }
@@ -14,19 +14,23 @@ class App extends Component<{}, IState> {
   constructor(props: {}) {
     super(props)
     this.state = {
-      searchTerm: localStorage.getItem('searchTerm') || '',
-      results: [],
+      result: null,
       isLoading: false,
       error: null
     }
   }
+  
+  onSearchResults = (data: ApiResponse) => {
+    if (!data) return
+
+    this.setState({ result: data })
+  }
+
   render() {
     return (
       <div className={styles.container}>
-        <Header />
-        <main className={styles.main}>
-          <Result />
-        </main>
+        <Header onSearchResults={this.onSearchResults} />
+        <Main result={this.state.result} />
       </div>
     )
   }
