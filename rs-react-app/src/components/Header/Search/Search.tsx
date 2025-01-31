3ab5c9e89b00a0eeadf069efd5fb62b581ad5909
DaniilRai13 <danik.ray.23@mail.ru>
2025-01-31 23:04:17 +0300

    feat: use prettier and move logic from handleSearch to parent function

--- rs-react-app/src/components/Header/Search/Search.tsx
+++ rs-react-app/src/components/Header/Search/Search.tsx
@@ -1,18 +1,17 @@
 import { Component, ChangeEvent } from 'react';
-import styles from './Search.module.scss'
-import { SwapiApiServices } from '../../../services/SwipApiServices';
-import { ApiResponse } from '../../../types/resultAPI.interface';
+import styles from './Search.module.scss';
+
 interface SearchState {
-  query: string | null
-  apiEndpoints: string[]
-  filteredEndpoints: string[]
-  isLoading: boolean
-  isFocus: boolean
-  error: string | null
+  query: string | null;
+  apiEndpoints: string[];
+  filteredEndpoints: string[];
+  isFocus: boolean;
+  error: string | null;
 }
 
 interface SearchProps {
-  onSearchResults: (data: ApiResponse) => void;
+  onSearchResults: (endpoint: string) => void;
+  isLoading: boolean;
 }
 
 class Search extends Component<SearchProps, SearchState> {
@@ -20,22 +19,35 @@ class Search extends Component<SearchProps, SearchState> {
     super(props);
     this.state = {
       query: localStorage.getItem('searchTerm'),
-      apiEndpoints: ['people/', 'planets/', 'films/', 'species/', 'vehicles/', 'starships/'],
-      filteredEndpoints: ['people/', 'planets/', 'films/', 'species/', 'vehicles/', 'starships/'],
+      apiEndpoints: [
+        'people/',
+        'planets/',
+        'films/',
+        'species/',
+        'vehicles/',
+        'starships/',
+      ],
+      filteredEndpoints: [
+        'people/',
+        'planets/',
+        'films/',
+        'species/',
+        'vehicles/',
+        'starships/',
+      ],
       isFocus: false,
-      isLoading: false,
       error: null,
     };
     this.handleSearch = this.handleSearch.bind(this);
   }
   componentDidMount(): void {
-    if (!this.state.query) return
-    this.handleSearch(this.state.query)
+    if (!this.state.query) return;
+    this.handleSearch(this.state.query);
   }
   handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const query = event.target.value.toLowerCase();
 
-    const filtered = this.state.apiEndpoints.filter(endpoint =>
+    const filtered = this.state.apiEndpoints.filter((endpoint) =>
       endpoint.toLowerCase().startsWith(query)
     );
 
@@ -43,37 +55,28 @@ class Search extends Component<SearchProps, SearchState> {
   };
 
   handleListShow = (): void => {
-    this.setState({ isFocus: true })
+    this.setState({ isFocus: true });
   };
 
   handleListBlur = () => {
     setTimeout(() => {
       this.setState({ isFocus: false });
     }, 100);
-    return
-  }
+  };
 
   handleSearch = async (endpoint: string): Promise<void> => {
-    if (!endpoint) {
-      alert('Пожалуйста, выберите запрос');
-      return;
-    }
-    console.log(endpoint)
-    this.setState({ isLoading: true, error: null, query: endpoint });
-
+    this.setState({ query: endpoint });
     try {
-      const data = await SwapiApiServices.get(endpoint)
-
-      this.props.onSearchResults(data);
+      this.props.onSearchResults(endpoint);
     } catch (error) {
-      this.setState({ error: error instanceof Error ? error.message : 'Unknown error' });
-    } finally {
-      this.setState({ isLoading: false });
+      this.setState({
+        error: error instanceof Error ? error.message : 'Unknown error',
+      });
     }
   };
 
   render() {
-    const { query, filteredEndpoints, isLoading, error, isFocus } = this.state;
+    const { query, filteredEndpoints, isFocus } = this.state;
 
     return (
       <div className={styles.searchContainer}>
@@ -87,27 +90,30 @@ class Search extends Component<SearchProps, SearchState> {
             onBlur={this.handleListBlur}
             placeholder="Начните вводить запрос"
           />
-          {isFocus && <div className={styles.list}>
-            {filteredEndpoints.length > 0 ? (
-              filteredEndpoints.map((endpoint, index) => (
-                <div
-                  key={index}
-                  className={styles.item}
-                  onClick={() => this.handleSearch(endpoint)}
-                >
-                  {endpoint}
-                </div>
-              ))
-            ) : (
-              <div>Нет доступных запросов</div>
-            )}
-          </div>}
+          {isFocus && (
+            <div className={styles.list}>
+              {filteredEndpoints.length > 0 ? (
+                filteredEndpoints.map((endpoint, index) => (
+                  <div
+                    key={index}
+                    className={styles.item}
+                    onClick={() => this.handleSearch(endpoint)}
+                  >
+                    {endpoint}
+                  </div>
+                ))
+              ) : (
+                <div>Нет доступных запросов</div>
+              )}
+            </div>
+          )}
         </label>
-        <button onClick={() => this.handleSearch(filteredEndpoints[0])} disabled={isLoading}>
-          {isLoading ? 'Загрузка...' : 'Search'}
+        <button
+          onClick={() => this.handleSearch(this.state.query || '')}
+          disabled={this.props.isLoading}
+        >
+          {this.props.isLoading ? 'Загрузка...' : 'Search'}
         </button>
-
-        {error && <div className={styles.error}>{error}</div>}
       </div>
     );
   }
