92776d6bdf22010a20fd3c039d48e7f7f7aab2dc
DaniilRai13 <danik.ray.23@mail.ru>
2025-01-31 18:43:52 +0300

    feat: create a new interface for header component

--- rs-react-app/src/components/Header/Header.tsx
+++ rs-react-app/src/components/Header/Header.tsx
@@ -1,14 +1,21 @@
 import { Component } from "react";
 import styles from './Header.module.scss'
 import Search from "./Search/Search";
-class Header extends Component {
+import { ApiResponse } from "../../types/resultAPI.interface";
+
+interface IHeader {
+  onSearchResults: (data: ApiResponse) => void
+}
+
+class Header extends Component<IHeader> {
   render() {
+    const { onSearchResults } = this.props
     return (
       <header className={styles.header}>
         <div className={styles.logoContainer}>
           <img alt="logo" src="https://img.icons8.com/bubbles/512/react.png" />
         </div>
-        <Search />
+        <Search onSearchResults={onSearchResults} />
       </header>
     )
   }
