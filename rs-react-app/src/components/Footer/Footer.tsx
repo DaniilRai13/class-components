eaab34da88993fc5251f6ba0100a3760892caa38
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-03 00:20:59 +0300

    feat: create Footer component

---
+++ rs-react-app/src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import styles from './Footer.module.scss';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.container}>
+          <p className={styles.text}>© 2025 Rai Daniil. All rights reserved.</p>
+          <div className={styles.links}>
+            <a href="https://t.me/DaniilRai" target="_blank" className={styles.link}>Telegram</a>
+            <a href="https://github.com/DaniilRai13/" target="_blank" className={styles.link}>GitHub</a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
