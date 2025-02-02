c605d5c8a8754419b2963f483f4db847a5d68090
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-02 17:07:58 +0300

    feat: create helper functions for localStorage interactions

---
+++ rs-react-app/src/shared/useLocalStorage.ts
@@ -0,0 +1,4 @@
+export const localStorageHelper = {
+  getFromLocalStorage: (name: string) => localStorage.getItem(name),
+  setToLocalStorage: (name: string, content: string) => localStorage.setItem(name, content)
+}
