1e9076107bead7d9862d2d25759dcf72979886e3
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-02 17:16:33 +0300

    feat: set is Loading prop to Header component

--- rs-react-app/src/components/Header/Header.tsx
+++ rs-react-app/src/components/Header/Header.tsx
@@ -1,21 +1,21 @@
 import { Component } from 'react';
 import styles from './Header.module.scss';
 import Search from './Search/Search';
-import { ApiResponse } from '../../types/resultAPI.interface';
 
 interface IHeader {
-  onSearchResults: (data: ApiResponse) => void;
+  onSearchResults: (endpoint: string) => void;
+  isLoading: boolean;
 }
 
 class Header extends Component<IHeader> {
   render() {
-    const { onSearchResults } = this.props;
+    const { onSearchResults,isLoading } = this.props;
     return (
       <header className={styles.header}>
         <div className={styles.logoContainer}>
           <img alt="logo" src="https://img.icons8.com/bubbles/512/react.png" />
         </div>
-        <Search onSearchResults={onSearchResults} />
+        <Search onSearchResults={onSearchResults} isLoading={isLoading}/>
       </header>
     );
   }
