1cebcd30db3caf12725ab976ffb7ab1c379c11eb
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-02 17:11:53 +0300

    feat: add classnames for blocks and set new data for Card component

--- rs-react-app/src/components/Result/CardList/CardList.tsx
+++ rs-react-app/src/components/Result/CardList/CardList.tsx
@@ -1,36 +1,35 @@
-import { Component } from "react";
-import { ApiResponse } from "../../../types/resultAPI.interface";
-import Card from "./Card/Card";
+import { Component } from 'react';
+import { ApiResponse } from '../../../types/resultAPI.interface';
+import Card from './Card/Card';
+import styles from './CardList.module.scss'
+import { localStorageHelper } from '../../../shared/useLocalStorage';
 
 interface ICardList {
-  result: ApiResponse | null
+  result: ApiResponse | null;
 }
-
 
 class CardList extends Component<ICardList> {
 
   render() {
-    const { result } = this.props
-
+    const { result } = this.props;
+    const searchTerm = localStorageHelper.getFromLocalStorage('searchTerm')
+    const itemName = `${searchTerm?.charAt(0).toUpperCase()}${searchTerm?.slice(1, searchTerm?.length)}`
     return (
       <>
-        <div>
-          <div>
-            <h4>Item name</h4>
-            <h4>Item description</h4>
+        <div className={styles.listContainer}>
+          <div className={styles.header}>
+            <h4 className={styles.name}>{itemName} name</h4>
+            <h4 className={styles.description}>{itemName} description</h4>
           </div>
-          <div>
-            {result?.results
-              && result.results.map((item, index) => (
-                <Card
-                  key={index}
-                  item={item}
-                  resultType={result.constructor.name} />
+          <div className={styles.list}>
+            {result?.results &&
+              result.results.map((item, index) => (
+                <Card key={index} item={item} searchTerm={searchTerm} />
               ))}
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
