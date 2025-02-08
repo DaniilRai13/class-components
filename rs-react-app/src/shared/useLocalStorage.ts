993f1be5fb1fbf2a89fef89d7bf39ef607e4985e
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-08 22:11:01 +0300

    feat: update function to custom hook

--- rs-react-app/src/shared/useLocalStorage.ts
+++ rs-react-app/src/shared/useLocalStorage.ts
@@ -1,5 +1,20 @@
-export const localStorageHelper = {
-  getFromLocalStorage: (name: string) => localStorage.getItem(name),
-  setToLocalStorage: (name: string, content: string) =>
-    localStorage.setItem(name, content),
+import { useState, useEffect } from 'react';
+
+export const useLocalStorage = (key: string, initialValue: string) => {
+  const [value, setValue] = useState<string>(initialValue);
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem(key);
+    if (storedValue) {
+      setValue(storedValue);
+    }
+  }, [key]);
+
+  const setLocalStorageValue = (newValue: string) => {
+    setValue(newValue);
+    localStorage.setItem(key, newValue);
+  };
+
+  return [value, setLocalStorageValue] as const;
 };
+
