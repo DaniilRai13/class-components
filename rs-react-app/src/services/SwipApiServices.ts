e382bd8ce105c7b0266f7a71a35e5284bb39be7d
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-08 21:42:36 +0300

    refactor: change the received data

--- rs-react-app/src/services/SwipApiServices.ts
+++ rs-react-app/src/services/SwipApiServices.ts
@@ -1,11 +1,25 @@
-import { ApiResponse } from '../types/resultAPI.interface';
+import { IPeople, IPeoples } from "../types/resultAPI.interface";
 
 export const SwapiApiServices = {
-  get: async (endpoint: string): Promise<ApiResponse> => {
+  get: async (endpoint: string): Promise<IPeoples> => {
     const response = await fetch(`https://swapi.dev/api/${endpoint}`);
     if (!response.ok) {
       throw new Error(`HTTP Error ${response.status}`);
     }
-    return await response.json();
+    const data = await response.json()
+
+    return {
+      ...data,
+      results: data.results.map((item: IPeople) => ({
+        name: item.name,
+        height: item.height,
+        mass: item.mass,
+        hair_color: item.hair_color,
+        skin_color: item.skin_color,
+        eye_color: item.eye_color,
+        birth_year: item.birth_year,
+        gender: item.gender,
+      }))
+    }
   },
 };
--- rs-react-app/src/types/resultAPI.interface.ts
+++ rs-react-app/src/types/resultAPI.interface.ts
@@ -8,4 +8,9 @@ export interface IPeople {
   name: string;
   height: string;
   mass: string;
+  hair_color: "blond",
+  skin_color: "fair",
+  eye_color: "blue",
+  birth_year: "19BBY",
+  gender: "male",
 }
