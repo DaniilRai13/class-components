2ea3505d5b2128ddfc34e46df41ad83ae8780693
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-09 22:56:51 +0300

    feat: implement function to open detail section

--- rs-react-app/src/components/Result/CardList/ListItem/ListItem.tsx
+++ rs-react-app/src/components/Result/CardList/ListItem/ListItem.tsx
@@ -5,12 +5,14 @@ import { IPeople } from '../../../../types/resultAPI.interface';
 interface IListItem {
   item: IPeople;
   searchTerm: string | null;
+  showDetails: (id: string) => void;
 }
 
-const ListItem: FC<IListItem> = ({ item }) => {
+const ListItem: FC<IListItem> = ({ item, showDetails }) => {
+  const id = item.url.split('/')[item.url.split('/').length - 2];
   return (
     <>
-      <div className={styles.item}>
+      <div className={styles.item} onClick={() => showDetails(id)}>
         <div className={styles.title}>{item.name}</div>
         <div className={styles.description}>
           Mass: {item.mass}, Height: {item.height}
--- rs-react-app/src/components/Result/Result.tsx
+++ rs-react-app/src/components/Result/Result.tsx
@@ -2,16 +2,25 @@ import { FC } from 'react';
 import { CardList } from './CardList/CardList';
 import { IPeoples } from '../../types/resultAPI.interface';
 import styles from './Result.module.scss';
-import { useNavigation, useParams, useSearchParams } from 'react-router';
+import { useSearchParams } from 'react-router';
 
 interface IResult {
   result: IPeoples | null;
   isLoading: boolean;
   onSearch: (endpoint: string) => void;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
 }
 
-export const Result: FC<IResult> = ({ result, isLoading, onSearch }) => {
+export const Result: FC<IResult> = ({
+  result,
+  isLoading,
+  onSearch,
+  isOpen,
+  setIsOpen,
+}) => {
   const [searchParams, setSearchParams] = useSearchParams();
+
   const currentPage = Number(searchParams.get('page')) || 1;
 
   const handlePageChange = async (newPage: number, page: string) => {
@@ -19,14 +28,27 @@ export const Result: FC<IResult> = ({ result, isLoading, onSearch }) => {
       onSearch(`people/?page=${newPage}`);
     else if (page === 'next' && result?.next)
       onSearch(`people/?page=${newPage}`);
+    setSearchParams((params) => {
+      return {
+        page: newPage.toString(),
+        ...(params.get('details') ? { details: params.get('details')! } : {}),
+      };
+    });
+  };
 
-    setSearchParams({ page: newPage.toString() });
+  const showDetails = (detailId: string) => {
+    setIsOpen(true);
+    setSearchParams({ page: currentPage.toString(), details: detailId });
   };
 
   return (
     <>
       <div className={styles.result}>
-        <CardList result={result} isLoading={isLoading} />
+        <CardList
+          result={result}
+          isLoading={isLoading}
+          showDetails={showDetails}
+        />
         <div className={styles.navigation}>
           <button
             className={styles.prev}
