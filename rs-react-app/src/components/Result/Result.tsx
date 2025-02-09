90c240aea92b34cde1c124e856e7ca84071128cc
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-09 20:59:10 +0300

    feat: add navigation

--- rs-react-app/src/components/Result/Result.tsx
+++ rs-react-app/src/components/Result/Result.tsx
@@ -2,17 +2,41 @@ import { FC } from 'react';
 import { CardList } from './CardList/CardList';
 import { IPeoples } from '../../types/resultAPI.interface';
 import styles from './Result.module.scss'
+import { useNavigation, useParams, useSearchParams } from 'react-router';
 
 interface IResult {
   result: IPeoples | null;
   isLoading: boolean;
+  onSearch: (endpoint: string) => void
 }
 
-export const Result: FC<IResult> = ({ result, isLoading }) => {
+export const Result: FC<IResult> = ({ result, isLoading, onSearch }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+
+  const handlePageChange = async (newPage: number, page: string) => {
+    if (page === 'prev' && result?.previous) onSearch(`people/?page=${newPage}`)
+    else if (page === 'next' && result?.next) onSearch(`people/?page=${newPage}`)
+
+    setSearchParams({ page: newPage.toString() });
+  };
+
   return (
     <>
       <div className={styles.result}>
         <CardList result={result} isLoading={isLoading} />
+        <div className={styles.navigation}>
+          <button
+            className={styles.prev}
+            onClick={() => handlePageChange(currentPage - 1, 'prev')}
+            disabled={isLoading || !result?.previous}
+          >prev</button>
+          <button
+            className={styles.next}
+            onClick={() => handlePageChange(currentPage + 1, 'next')}
+            disabled={isLoading || !result?.next}
+          >next</button>
+        </div>
       </div>
     </>
   );
