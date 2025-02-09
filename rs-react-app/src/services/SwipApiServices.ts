d136536464a185078fd7c35622a579eb81c2bd8a
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-09 20:59:47 +0300

    feat: use axios instead fetch and add new method getPeopleById

--- rs-react-app/src/services/SwipApiServices.ts
+++ rs-react-app/src/services/SwipApiServices.ts
@@ -1,16 +1,37 @@
+import axios from 'axios';
 import { IPeople, IPeoples } from '../types/resultAPI.interface';
+
+const api = axios.create({
+  baseURL: 'https://swapi.dev/api/'
+});
+
 
 export const SwapiApiServices = {
   get: async (endpoint: string): Promise<IPeoples> => {
-    const response = await fetch(`https://swapi.dev/api/${endpoint}`);
-    if (!response.ok) {
-      throw new Error(`HTTP Error ${response.status}`);
+    try {
+      const response = await api.get(endpoint);
+      return {
+        ...response.data,
+        results: response.data.results.map((item: IPeople) => ({
+          name: item.name,
+          height: item.height,
+          mass: item.mass,
+          hair_color: item.hair_color,
+          skin_color: item.skin_color,
+          eye_color: item.eye_color,
+          birth_year: item.birth_year,
+          gender: item.gender,
+        })),
+      };
+    } catch (error) {
+      throw new Error('Bad request')
     }
-    const data = await response.json();
-
-    return {
-      ...data,
-      results: data.results.map((item: IPeople) => ({
+  },
+  getPeopleById: async (id: number): Promise<IPeople> => {
+    try {
+      const response = await api.get(`people/${id}`);
+      const item = response.data
+      return {
         name: item.name,
         height: item.height,
         mass: item.mass,
@@ -19,7 +40,20 @@ export const SwapiApiServices = {
         eye_color: item.eye_color,
         birth_year: item.birth_year,
         gender: item.gender,
-      })),
-    };
-  },
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        const { status } = error.response;
+        if (status === 404) {
+          throw new Error('Character not found (404)');
+        } else if (status === 500) {
+          throw new Error('Server error (500)');
+        } else {
+          throw new Error(`Request error (${status})`);
+        }
+      } else {
+        throw new Error('Unknown network error');
+      }
+    }
+  }
 };
