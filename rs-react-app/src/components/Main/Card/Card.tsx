d1698ee00732e46046fbadefa17eba60a14a79bb
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-09 21:00:20 +0300

    feat: display people info by Id

--- rs-react-app/src/components/Main/Card/Card.tsx
+++ rs-react-app/src/components/Main/Card/Card.tsx
@@ -1,31 +1,37 @@
-import { FC } from 'react'
-import { IPeople } from '../../../types/resultAPI.interface'
+import { FC, useEffect } from 'react'
 import styles from './Card.module.scss'
+import { useSearchParams } from 'react-router';
+import { useFetchSwap } from '../../../services/useFetchSwip';
 
-interface ICard {
-  result: IPeople | undefined;
-}
+const Card: FC = () => {
+  const [searchParams] = useSearchParams();
+  const detailsId = searchParams.get('details');
+  const { getPeople, peopleResult } = useFetchSwap()
 
-const Card: FC<ICard> = ({ result }) => {
+  useEffect(() => {
+    if (detailsId) getPeople(+detailsId)
+
+  }, [detailsId])
+
   return (
     <section className={styles.card}>
       <div className={styles.cardInner}>
-        <h2 className={styles.name}>{result?.name}</h2>
+        <h2 className={styles.name}>{peopleResult?.name}</h2>
         <div className={styles.mainInfo}>
           <h3 className={styles.mainTitle}>Main Info:</h3>
           <div className={styles.mainInfoContainer}>
-            <div className={styles.item}><strong>Gender:</strong> {result?.gender}</div>
-            <div className={styles.item}><strong>Height:</strong>{result?.height}</div>
-            <div className={styles.item}><strong>Mass:</strong>{result?.mass}</div>
-            <div className={styles.item}><strong>Birth Year:</strong>{result?.birth_year}</div>
+            <div className={styles.item}><strong>Gender:</strong> {peopleResult?.gender}</div>
+            <div className={styles.item}><strong>Height:</strong>{peopleResult?.height}</div>
+            <div className={styles.item}><strong>Mass:</strong>{peopleResult?.mass}</div>
+            <div className={styles.item}><strong>Birth Year:</strong>{peopleResult?.birth_year}</div>
           </div>
         </div>
         <div className={styles.secondInfo}>
           <h5 className={styles.secondTitle}>Second Info:</h5>
           <div className={styles.secondInfoContainer}>
-            <div className={styles.item}><strong>Hair:</strong>{result?.hair_color}</div>
-            <div className={styles.item}><strong>Skin:</strong>{result?.skin_color}</div>
-            <div className={styles.item}><strong>Eye:</strong>{result?.eye_color}</div>
+            <div className={styles.item}><strong>Hair:</strong>{peopleResult?.hair_color}</div>
+            <div className={styles.item}><strong>Skin:</strong>{peopleResult?.skin_color}</div>
+            <div className={styles.item}><strong>Eye:</strong>{peopleResult?.eye_color}</div>
           </div>
         </div>
       </div>
