b55663cbc5eeca50264423a0e235567a37d8eccc
DaniilRai13 <danik.ray.23@mail.ru>
2025-02-09 21:05:27 +0300

    feat: display person info using outlet

--- rs-react-app/src/components/Main/Main.tsx
+++ rs-react-app/src/components/Main/Main.tsx
@@ -1,17 +1,27 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import styles from './Main.module.scss';
 import { Result } from '../Result/Result';
 import { IPeoples } from '../../types/resultAPI.interface';
+import { Outlet, useSearchParams } from 'react-router';
 
 interface IMain {
   result: IPeoples | null;
   isLoading: boolean;
+  onSearch: (endpoint: string) => void
 }
 
-const Main: FC<IMain> = ({ result, isLoading }) => {
+const Main: FC<IMain> = ({ result, isLoading, onSearch }) => {
+  const [searchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+  
+  useEffect(() => {
+    onSearch(`people/?page=${currentPage}`);
+  }, []);
+
   return (
     <main className={styles.main}>
-      <Result result={result} isLoading={isLoading} />
+      <Result result={result} isLoading={isLoading} onSearch={onSearch} />
+      <Outlet />
     </main>
   );
 };
