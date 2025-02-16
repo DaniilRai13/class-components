c373b37176f0a31264615ff9341e13be7382d557
DaniilRai13 <testnet02@mail.ru>
2025-02-17 00:48:57 +0300

    feat: use useGetPeopleById query to get people info and stopped using closeDetails function

--- rs-react-app/src/components/Main/Card/Card.tsx
+++ rs-react-app/src/components/Main/Card/Card.tsx
@@ -1,54 +1,34 @@
-import { FC, useEffect, useState } from 'react';
-import styles from './Card.module.scss';
+import { FC } from 'react';
 import { useSearchParams } from 'react-router';
-import { useFetchSwap } from '../../../services/useFetchSwip';
+import styles from './Card.module.scss';
+import { useGetPeopleByIdQuery } from '../../../store/people/peopleApi';
 
-const Card: FC<{ closeDetail: (isOpen: boolean) => void }> = ({
-  closeDetail,
-}) => {
+const Card: FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const detailsId = searchParams.get('details');
-  const { getPeople, peopleResult } = useFetchSwap();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { data: people, isLoading } = useGetPeopleByIdQuery(detailsId || '');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (detailsId) {
-        setIsLoading(true);
-        try {
-          await getPeople(+detailsId); // Ожидаем завершения вызова getPeople
-        } catch (error) {
-          console.error('Error fetching data', error);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-    };
-
-    fetchData(); // Вызываем асинхронную функцию
-  }, [detailsId]);
-
-  return !isLoading ? (
+  return people ? (
     <section className={styles.card}>
       <div className={styles.cardInner}>
-        <h2 className={styles.name}>{peopleResult?.name}</h2>
+        <h2 className={styles.name}>{people?.name}</h2>
         <div className={styles.mainInfo}>
           <h3 className={styles.mainTitle}>Main Info:</h3>
           <div className={styles.mainInfoContainer}>
             <div className={styles.item}>
-              <strong>Gender:</strong> {peopleResult?.gender}
+              <strong>Gender:</strong> {people?.gender}
             </div>
             <div className={styles.item}>
               <strong>Height:</strong>
-              {peopleResult?.height}
+              {people?.height}
             </div>
             <div className={styles.item}>
               <strong>Mass:</strong>
-              {peopleResult?.mass}
+              {people?.mass}
             </div>
             <div className={styles.item}>
               <strong>Birth Year:</strong>
-              {peopleResult?.birth_year}
+              {people?.birth_year}
             </div>
           </div>
         </div>
@@ -57,22 +37,21 @@ const Card: FC<{ closeDetail: (isOpen: boolean) => void }> = ({
           <div className={styles.secondInfoContainer}>
             <div className={styles.item}>
               <strong>Hair:</strong>
-              {peopleResult?.hair_color}
+              {people?.hair_color}
             </div>
             <div className={styles.item}>
               <strong>Skin:</strong>
-              {peopleResult?.skin_color}
+              {people?.skin_color}
             </div>
             <div className={styles.item}>
               <strong>Eye:</strong>
-              {peopleResult?.eye_color}
+              {people?.eye_color}
             </div>
           </div>
         </div>
       </div>
       <button
         onClick={() => {
-          closeDetail(false);
           setSearchParams((params) => ({
             ...(params.get('page') ? { page: params.get('page')! } : {}),
           }));
@@ -81,9 +60,9 @@ const Card: FC<{ closeDetail: (isOpen: boolean) => void }> = ({
         Close Details
       </button>
     </section>
-  ) : (
+  ) : isLoading ? (
     <div>Loading....</div>
-  );
+  ) : null;
 };
 
 export default Card;
