73b9a8051f06fd3ed9dc7beaf581e507ff72b4f9
DaniilRai13 <testnet02@mail.ru>
2025-02-17 21:55:45 +0300

    refactor: change server port

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5001,
+    open: true,
+  },
   test: {
-    globals: true,  // Подключает глобальные функции тестирования (describe, it, expect)
-    environment: 'jsdom', // Эмулирует браузер
-    setupFiles: './src/__tests__/setup.ts', // Файл с настройками перед тестами
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.ts',
     coverage: {
-      provider: 'istanbul', // Покрытие кода тестами
-      include: ['src/**/*.tsx'], // Включить в отчёт только `.tsx` файлы
-      exclude: ['**/node_modules/**', '**/*.test.tsx', '**/*.spec.tsx', 'src/__tests__/setup.ts'],
+      provider: 'istanbul',
+      include: ['src/**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
     },
   },
 });
