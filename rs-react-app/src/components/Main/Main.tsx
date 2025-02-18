6e626fdf6014cedb96465de8009736003d7d4452
DaniilRai13 <testnet02@mail.ru>
2025-02-19 00:41:41 +0300

    feat: remove props and handling 'page' param

--- rs-react-app/src/components/Main/Main.tsx
+++ rs-react-app/src/components/Main/Main.tsx
@@ -1,36 +1,28 @@
 import { FC, MouseEvent, useEffect } from 'react'
 import { Outlet, useSearchParams } from 'react-router'
-import { IPeoples } from '../../types/resultAPI.interface'
 import { Result } from '../Result/Result'
 import styles from './Main.module.scss'
 
-interface IMain {
-  result: IPeoples | null
-  isLoading: boolean
-  onSearch: (endpoint: string) => void
-  isOpen: boolean
-  setIsOpen: (isOpen: boolean) => void
-}
-
-const Main: FC<IMain> = ({ result, isLoading, onSearch }) => {
+const Main: FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const currentPage = Number(searchParams.get('page')) || 1
   const currentDetails = searchParams.get('details')
-
-  useEffect(() => {
-    onSearch(`people/?page=${currentPage}`)
-  }, [])
 
   const closeDetails = (e: MouseEvent) => {
     if ((e.target as HTMLElement).classList.contains(styles.main)) {
       setSearchParams({ page: currentPage.toString() })
     }
   }
-
+  useEffect(() => {
+    const page = searchParams.get('page');
+    if (!page) {
+      setSearchParams({});
+    }
+  },[searchParams, setSearchParams]);
   return (
     <main className={styles.main} onClick={(e) => closeDetails(e)}>
-      <Result result={result} isLoading={isLoading} onSearch={onSearch} />
+      <Result />
       {currentDetails && <Outlet />}
     </main>
   )
