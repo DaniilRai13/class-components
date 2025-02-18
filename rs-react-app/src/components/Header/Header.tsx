c301c9020005c3a901d2fc67009ef9b00ecc56ef
DaniilRai13 <testnet02@mail.ru>
2025-02-19 00:35:37 +0300

    refactor: remove props

--- rs-react-app/src/components/Header/Header.tsx
+++ rs-react-app/src/components/Header/Header.tsx
@@ -2,18 +2,13 @@ import { FC } from 'react';
 import styles from './Header.module.scss';
 import Search from './Search/Search';
 
-interface IHeader {
-  onSearchResults: (endpoint: string) => void;
-  isLoading: boolean;
-}
-
-const Header: FC<IHeader> = ({ onSearchResults, isLoading }) => {
+const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <img alt="logo" src="https://img.icons8.com/bubbles/512/react.png" />
       </div>
-      <Search onSearchResults={onSearchResults} isLoading={isLoading} />
+      <Search />
     </header>
   );
 };
