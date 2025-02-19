efb4eeb061ea56a5ece7f66aa7eea9b72f48cb62
DaniilRai13 <testnet02@mail.ru>
2025-02-19 23:42:57 +0300

    feat: handle getListOfPeople from peopleApi and create new action(removeItems), and fix code in toggleMarkedPeoples

--- rs-react-app/src/store/people/peopleSlice.ts
+++ rs-react-app/src/store/people/peopleSlice.ts
@@ -1,27 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { IPeople } from '../../types/resultAPI.interface';
+import { createSlice } from '@reduxjs/toolkit'
+import { IPeople } from '../../types/resultAPI.interface'
+import { peopleApi } from './peopleApi'
+
+interface IMarkedPeople extends IPeople {
+  isChecked: boolean
+}
 interface IInitialState {
-  markedPeoples: IPeople[];
+  peoples: IPeople[];
+  markedPeoples: IMarkedPeople[];
   isOpenDetails: boolean;
+  isLoading: boolean;
 }
 const initialState: IInitialState = {
+  peoples: [],
   markedPeoples: [],
   isOpenDetails: false,
-};
+  isLoading: true
+}
 
 export const peopleSlice = createSlice({
   name: 'people',
   initialState,
   reducers: {
-    toggleMarkedPeoples: (state, { payload: id }) => {
-      state.markedPeoples.forEach((people) => {
-        if (people.id === id) {
-          state.markedPeoples.filter((item) => item.id !== id);
-        }
-      });
+    toggleMarkedPeoples: (state, { payload }: { payload: IPeople }) => {
+      const index = state.markedPeoples.findIndex((item) => item.id === payload.id)
+      if (index !== -1) {
+        state.markedPeoples.splice(index, 1)
+      } else {
+        state.markedPeoples.push({ ...payload, isChecked: true })
+      }
     },
+    removeItems: ((state) => {
+      state.markedPeoples = []
+    })
   },
-});
+  extraReducers(builder) {
+    builder.addMatcher(peopleApi.endpoints.getListPeoples.matchPending, (state) => {
+      state.isLoading = true
+    })
+    builder.addMatcher(peopleApi.endpoints.getListPeoples.matchFulfilled, (state, { payload }) => {
+      state.peoples = payload.results
+      state.isLoading = false
+    })
+  },
+})
 
-export const { toggleMarkedPeoples } = peopleSlice.actions;
-export default peopleSlice.reducer;
+export const { toggleMarkedPeoples, removeItems } = peopleSlice.actions
+export default peopleSlice.reducer
