48da75ace2624bfae7ca019e2f67bcc39fc1f87f
DaniilRai13 <testnet02@mail.ru>
2025-02-19 23:43:33 +0300

    feat: create function to convert items data to csv file

---
+++ rs-react-app/src/shared/convertToCSV.ts
@@ -0,0 +1,8 @@
+import { IPeople } from '../types/resultAPI.interface';
+
+export const convertToCSV = (items: IPeople[]): string => {
+	const headers: (keyof IPeople)[] = ["name", "mass", "gender", "url",'birth_year'];
+	const rows = items.map(item => headers.map((h) => item[h]).join(';'));
+
+	return [headers.join(';'), ...rows].join("\n");
+}
