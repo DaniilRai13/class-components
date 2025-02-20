049ce7e696568de5c0e23dfa9f485225a9a54cd2
DaniilRai13 <testnet02@mail.ru>
2025-02-20 21:13:36 +0300

    feat: implement ThemeProvider

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -6,17 +6,20 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import NotFound from './components/NotFound/NotFound.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
+import ThemeProvider from './providers/ThemeProvider/ThemeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/people/*" element={<App />} />
-          <Route path="/" element={<Navigate to="/people" />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/people/*" element={<App />} />
+            <Route path="/" element={<Navigate to="/people" />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
