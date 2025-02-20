e4b19c88bc6f026f6ba588e169d27cfc29a0eb78
DaniilRai13 <testnet02@mail.ru>
2025-02-20 23:47:27 +0300

    feat: add basename

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
-import NotFound from './components/NotFound/NotFound.tsx';
 import { Provider } from 'react-redux';
-import { store } from './store/store.ts';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+import App from './App.tsx';
+import NotFound from './components/NotFound/NotFound.tsx';
+import './index.css';
 import ThemeProvider from './providers/ThemeProvider/ThemeProvider.tsx';
+import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeProvider>
-        <BrowserRouter>
+        <BrowserRouter basename='/class-components/'>
           <Routes>
             <Route path="/people/*" element={<App />} />
             <Route path="/" element={<Navigate to="/people" />} />
