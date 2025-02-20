228e684a934f662d7450464cc321ef1d3677c70c
DaniilRai13 <testnet02@mail.ru>
2025-02-20 22:51:38 +0300

    feat: use useThemeValues to get theme value

--- rs-react-app/src/components/Main/SelectedItems/SelectedItems.tsx
+++ rs-react-app/src/components/Main/SelectedItems/SelectedItems.tsx
@@ -5,14 +5,16 @@ import styles from './SelectedItems.module.scss';
 import { useActions } from '../../hooks/useActions';
 import { IPeople } from '../../../types/resultAPI.interface';
 import { convertToCSV } from '../../../shared/convertToCSV';
+import { useThemeValues } from '../../../providers/ThemeProvider/useTheme';
 const SelectedItems: FC = () => {
-  const { markedPeoples } = useTypedSelector(({ people }) => people);
-  const { removeItems } = useActions();
-  const isDisabled = markedPeoples.length;
-
-  const downloadCSV = (peoples: IPeople[]) => {
-    const csvContent = convertToCSV(peoples);
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+	const { markedPeoples } = useTypedSelector(({ people }) => people);
+	const { removeItems } = useActions();
+	const isDisabled = markedPeoples.length;
+	const theme = useThemeValues()
+	
+	const downloadCSV = (peoples: IPeople[]) => {
+		const csvContent = convertToCSV(peoples);
+		const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
 
     const link = document.createElement('a');
     const fileName = `${peoples.length}_episodes.csv`;
@@ -21,40 +23,40 @@ const SelectedItems: FC = () => {
     link.click();
   };
 
-  return (
-    <>
-      {!!isDisabled && (
-        <section className={styles.selected}>
-          <div className={styles.header}>
-            <h3 className={styles.title}>
-              Selected {markedPeoples.length} Items:
-            </h3>
-            <div className={styles.buttons}>
-              <button
-                className={styles.unselect}
-                disabled={!isDisabled}
-                onClick={() => removeItems()}
-              >
-                Unselect all
-              </button>
-              <button
-                className={styles.download}
-                disabled={!isDisabled}
-                onClick={() => downloadCSV(markedPeoples)}
-              >
-                Download
-              </button>
-            </div>
-          </div>
-          <div className={styles.list}>
-            {markedPeoples.map((item) => (
-              <ListItem key={item.url} item={item} />
-            ))}
-          </div>
-        </section>
-      )}
-    </>
-  );
+	return (
+		<>
+			{!!isDisabled && (
+				<section className={styles.selected} data-theme={theme === 'light' ? 'light' : 'dark'}>
+					<div className={styles.header}>
+						<h3 className={styles.title}>
+							Selected {markedPeoples.length} Items:
+						</h3>
+						<div className={styles.buttons}>
+							<button
+								className={styles.unselect}
+								disabled={!isDisabled}
+								onClick={() => removeItems()}
+							>
+								Unselect all
+							</button>
+							<button
+								className={styles.download}
+								disabled={!isDisabled}
+								onClick={() => downloadCSV(markedPeoples)}
+							>
+								Download
+							</button>
+						</div>
+					</div>
+					<div className={styles.list}>
+						{markedPeoples.map((item) => (
+							<ListItem key={item.url} item={item} />
+						))}
+					</div>
+				</section>
+			)}
+		</>
+	);
 };
 
 export default SelectedItems;
