6e16399323639baae072da6ebb0f863c8a57da5c
DaniilRai13 <testnet02@mail.ru>
2025-02-20 21:13:52 +0300

    feat: create ThemeProvider

---
+++ rs-react-app/src/providers/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode, useCallback, useMemo, useState } from 'react';
+import { IThemeValuesTypes, ThemeActionsContext, ThemeValuesContext } from './useTheme';
+
+const ThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
+	const [theme, setTheme] = useState<IThemeValuesTypes>('dark');
+	console.log(theme)
+
+	const handleTheme = useCallback((value: IThemeValuesTypes) => {
+		setTheme(value)
+	}, [])
+
+	const actions = useMemo(() => ({ handleTheme }), [handleTheme])
+
+	return (
+		<ThemeValuesContext.Provider value={theme}>
+			<ThemeActionsContext.Provider value={actions}>
+				{children}
+			</ThemeActionsContext.Provider>
+		</ThemeValuesContext.Provider >
+	)
+}
+
+export default ThemeProvider
