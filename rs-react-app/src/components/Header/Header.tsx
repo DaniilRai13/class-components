3f77878a51caf50c7c818b4ee97581c0cc27a7eb
DaniilRai13 <testnet02@mail.ru>
2025-02-20 21:51:43 +0300

    feat: implement toggle theme component

--- rs-react-app/src/components/Header/Header.tsx
+++ rs-react-app/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import styles from './Header.module.scss';
 import Search from './Search/Search';
+import ToggleTheme from './ToggleTheme/ToggleTheme';
 
 const Header: FC = () => {
   return (
@@ -8,6 +9,7 @@ const Header: FC = () => {
       <div className={styles.logoContainer}>
         <img alt="logo" src="https://img.icons8.com/bubbles/512/react.png" />
       </div>
+      <ToggleTheme />
       <Search />
     </header>
   );
