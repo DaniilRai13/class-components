65413217ebda45771e98ed262b354865d070fdd7
DaniilRai13 <testnet02@mail.ru>
2025-02-20 23:06:21 +0300

    feat: handle theme in links

--- rs-react-app/src/components/Footer/Footer.tsx
+++ rs-react-app/src/components/Footer/Footer.tsx
@@ -1,15 +1,15 @@
 import { FC } from 'react';
-import styles from './Footer.module.scss';
 import { useThemeValues } from '../../providers/ThemeProvider/useTheme';
+import styles from './Footer.module.scss';
 
 const Footer: FC = () => {
   const theme = useThemeValues()
-  
+
   return (
     <footer className={styles.footer} data-theme={theme === 'light' ? 'light' : 'dark'}>
       <div className={styles.container}>
         <p className={styles.text}>© 2025 Rai Daniil. All rights reserved.</p>
-        <div className={styles.links}>
+        <div className={styles.links} data-theme={theme === 'light' ? 'light' : 'dark'}>
           <a
             href="https://t.me/DaniilRai"
             rel="noreferrer"
