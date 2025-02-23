2f483e0bfbb6d6403f528402a5bf850330a864b2
DaniilRai13 <testnet02@mail.ru>
2025-02-24 01:37:43 +0300

    refactor: move to separate obj

--- rs-react-app/src/store/store.ts
+++ rs-react-app/src/store/store.ts
@@ -1,14 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { peopleApi } from './people/peopleApi';
 import peopleReducer from './people/peopleSlice';
 import errorReducer from './errorSlice';
-
-export const store = configureStore({
-  reducer: {
+export const rootReducer = combineReducers(
+  {
     errorReducer: errorReducer,
     people: peopleReducer,
     [peopleApi.reducerPath]: peopleApi.reducer,
-  },
+  }
+)
+
+export const store = configureStore({
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(peopleApi.middleware),
 });
