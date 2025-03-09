178a40b1f151850855692163770acf43f13f1203
DaniilRai13 <testnet02@mail.ru>
2025-03-09 22:30:23 +0300

    feat: create helper function to create query string

---
+++ rs-react-app/app/utils/createQueryString.ts
@@ -0,0 +1,5 @@
+export const createQueryString = (searchParams: URLSearchParams, name: string, value: string) => {
+  const params = new URLSearchParams(searchParams.toString());
+  params.set(name, value);
+  return params.toString(); 
+};
