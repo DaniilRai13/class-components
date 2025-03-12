397544607c9295189159c45491fe3b36c09c681b
DaniilRai13 <testnet02@mail.ru>
2025-03-12 22:20:17 +0300

    feat: create store

---
+++ forms/src/store/store.ts
@@ -0,0 +1,8 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const reducers = combineReducers({});
+export const store = configureStore({
+  reducer: reducers,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
