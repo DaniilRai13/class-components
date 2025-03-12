46134b504ea61d3a5cb48c3adf1beb34fbcee611
DaniilRai13 <testnet02@mail.ru>
2025-03-12 22:22:03 +0300

    feat: create common header component

---
+++ forms/src/components/ui/heading/Heading.tsx
@@ -0,0 +1,15 @@
+import cn from 'classnames';
+import { FC, ReactNode } from 'react';
+import styles from './Heading.module.scss';
+
+interface IHeading {
+  children?: ReactNode;
+  className?: string;
+}
+
+const Heading: FC<IHeading> = ({ children, className }) => {
+  console.log(children);
+  return <h3 className={cn(styles.heading, className)}>{children}</h3>;
+};
+
+export default Heading;
