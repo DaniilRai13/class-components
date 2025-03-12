d9ab693bc401546b59ac40dfb366d274ac4d96ef
DaniilRai13 <testnet02@mail.ru>
2025-03-12 22:22:34 +0300

    feat: create common Field component

---
+++ forms/src/components/ui/form/Field.tsx
@@ -0,0 +1,22 @@
+import cn from 'classnames';
+import { forwardRef } from 'react';
+import styles from './field.module.scss';
+import { IField } from './form.interface';
+
+const Field = forwardRef<HTMLInputElement, IField>(
+  ({ error, className, placeholder, type = 'text', style, ...rest }, ref) => {
+    return (
+      <div className={cn(styles.field, className)} style={style}>
+        <label>
+          <span>{placeholder}</span>
+          <input ref={ref} type={type} {...rest} />
+        </label>
+        {error && <div className={styles.error}>{error.message}</div>}
+      </div>
+    );
+  }
+);
+
+Field.displayName = 'Field';
+
+export default Field;
